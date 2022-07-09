bbdb4a7926029bb5de63a9a5990230d0b992c4a0
Soporte Sistemas 3 <soporte.sistemas3@sto.com.mx>
2022-07-09 11:23:42 -0500

    Agregamos un ejemplo de callback

--- src/callback/index.js
+++ src/callback/index.js
@@ -8,10 +8,6 @@ const multiply = (num1, num2) => {
     return num1 * num2;
 };
 
-// Funcion Callback
-const calc = (num1, num2, callback) => {
-return callback(num1, num2);
-};
 
 // Ejecutando la funcion callback con suma y multiplicacion
 console.log(`Suma de 2 numeros: ${calc(3, 2, sum)}`);
@@ -35,3 +31,22 @@ const greeting = (name) => {
 };
 
 setTimeout(greeting, 2000, 'Victor'); // Recibe como parametros: (funcion, tiempo que tardara en ejecutarse, parametros que necesita la funcion)
+
+
+
+
+
+
+
+// EJEMPLO DE CALLBACK
+const calc = (num1, num2, callback) => {
+return callback(num1, num2);
+};
+
+const calculo = (num1, num2, callback, callback2) => {
+    console.log(`Los resultados de las operaciones son: Suma: ${callback(num1, num2)}, 
+    Multiplicacion: ${callback2(num1, num2)}`);
+    return sum, multiply;
+};
+
+console.log(calculo(3, 5, sum, multiply));
