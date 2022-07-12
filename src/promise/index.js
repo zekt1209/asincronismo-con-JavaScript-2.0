84330f35d1023420dd52d9e2adc7bd021a9afcdd
Soporte Sistemas 3 <soporte.sistemas3@sto.com.mx>
2022-07-12 15:13:06 -0500

    Pequeños detalles en promesas/index.js

--- src/promise/index.js
+++ src/promise/index.js
@@ -2,6 +2,8 @@ const promise = new Promise(function (resolve, reject) {
     resolve('Hey!');
 });
 
+// Ejemplo con promesas, saber si tenemos las suficientes vacas
+
 const cows = 15;
 
 const countCows = new Promise (function (resolve, reject) {
@@ -14,4 +16,62 @@ const countCows = new Promise (function (resolve, reject) {
 
 countCows.then((result) => {
     console.log(result);
+}).catch((error) => {
+    console.log(error);
+}).finally(() => {
+    console.log('Finally');
+});
+
+
+// Ejemplo con promises y objetos:
+// imprimir los nombres de personas mayores de edad y decir quienes son menores de edad.
+
+
+const personas = [
+    {
+        'nombre': 'Carlos',
+        'edad': '30',
+        'country': 'Mexico',
+    },
+    {
+        'nombre': 'Paty',
+        'edad': '26',
+        'country': 'Mexico',
+    },
+    {
+        'nombre': 'Paulina',
+        'edad': '29',
+        'country': 'Colombia',
+    },
+    {
+        'nombre': 'Rubi',
+        'edad': '17',
+        'country': 'Ecuador',
+    },
+];
+
+const countPeople = new Promise((resolve, reject) => {
+    let personasAceptadas = [];
+    let personasNoAceptadas = [];
+
+    personas.forEach(element => {
+        if (parseInt(element.edad) < 18) {
+            personasNoAceptadas.push(element.nombre);
+        } else {
+            personasAceptadas.push(element.nombre);
+        }
+    });
+
+        let nombresNoAceptados = personasNoAceptadas.join(', ');
+        let nombresAceptados = personasAceptadas.join(', ');
+
+        resolve(`SI pueden entrar: ${nombresAceptados}, NO pueden entrar: ${nombresNoAceptados}`);
+        reject(`Hey. algo salio mal! `);
+});
+
+countPeople.then((result) => {
+    console.log(result);
 })
+.catch((error) => {
+    console.log(error);
+});
