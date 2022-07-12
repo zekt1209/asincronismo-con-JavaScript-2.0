b9a73089513e8d6b70947f972f6cbf3dcf2194bc
Soporte Sistemas 3 <soporte.sistemas3@sto.com.mx>
2022-07-12 12:46:50 -0500

    Terminamos la funcion de fetchData y hicimos nuestros primeros llamados con callbacks, Lo ejecutamos por consola y modificamos el archivo package.json para ejecutarlo como script en terminal (npm)

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -3,27 +3,34 @@ const API = 'https://api.escuelajs.co/api/v1';  // Guardamos la url de la API do
 
 
 const fetchData = (urlApi, callback) => {
-    let xhttp = new XMLHttprequest();
+    let xhttp = new XMLHttprequest(); // La referencia a XMLHttprequest
 
     xhttp.open('GET', urlApi, true); // Abrimos la conexion y le pasamos el METODO que aplicaremos, la URL que le pasaremos y TRUE
-    xhttp.onreadystatechange = function (event) {
-        if (xhttp.readyState === 4) {
-            if (xhttp.status === 200) {
-                callback(null, JSON.parse(xhttp.responseText));
+    xhttp.onreadystatechange = function (event) {   // Escuchamos por los cambios que pueda llegar a tener
+        if (xhttp.readyState === 4) {   // Validar el estado dentro de los que tenemos
+            if (xhttp.status === 200) { // Validamos el Status que nos entrega el servidor
+                callback(null, JSON.parse(xhttp.responseText)); // Recibimos un responseText
+            }else {
+                const error = new Error('Error' + urlApi); // Mandamos un error en caso de que el servidor nos entregue algun estado diferente al 200
+                return callback(error, null);
             }
-        } else {
-            const error = new Error('Error' + urlApi);
-            return callback(error, null);
-        }
+        } 
     }
     xhttp.send();
 }
+
+// Llamamos a la funcion fetchData y le indicamos que datos son los que queremos traer
 
 fetchData(`${API}/products`, function (error1, data1) {
     if (error1) return console.error(error1);
         fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
             if (error2) return console.log(error2);
-                fetchData(`${API}/categories/${data2?.category?.id}`)
-        })
-})
+                fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+                    if (error3) return console.error(error3);
+                    console.log(data1[0]);  // Imprimimos la informacion deseada que previamente nos trajimos 
+                    console.log(data2.title);
+                    console.log(data3.name);
+                });
+        });
+});
 
