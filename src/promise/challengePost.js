84d70534bf5baff2971f51ea499680933b8b74ce
Soporte Sistemas 3 <soporte.sistemas3@sto.com.mx>
2022-07-13 14:36:56 -0500

    Se termino video de Promises, toca empezar con funciones asincronas

---
+++ src/promise/challengePost.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'; // El uso de fetch
+const API = 'https://api.escuelajs.co/api/v1';  // El URL de nuestra API
+
+const postData = (urlApi, data) => {    // Funcion que se va a encargar de usar fetch y transformarlo al llamado del metodo POST con las configuraciones minimas que podemos agregar y luego retornarlo
+    const response = fetch(urlApi, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response;
+};
+
+const data = {      // Data que queremos almacenar
+    "title": "New Sneakers!!! ",
+    "price": 1199,
+    "description": "The most amazing sneakers",
+    "categoryId": 1,
+    "images": ["https://placeimg.com/640/480/any"]
+}
+
+postData(`${API}/products`, data) // Podemos utilizar postData como una promesa para poder usar el then
+.then(response => response.json())  // Transformamos la respuesta en un objeto JSON
+.then(data => console.log(data)) // Mostramos la informacion en la consola
