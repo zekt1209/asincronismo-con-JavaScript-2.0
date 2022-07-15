bc66ad7a4c802ef0bcf6af8348ccc60612b3923b
victor <victor_27edge@hotmail.com>
2022-07-15 03:19:50 -0500

    Progreso: Implementacion de funciones asincronas, podemos continuar con try and catch

---
+++ src/async/index.js
@@ -0,0 +1,22 @@
+// async await
+
+const fnAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true) 
+            ? setTimeout(() => {resolve('Hello Async World!')}, 2000)
+            : reject(new Error('Error!'));
+    });
+};
+
+const anotherFunction = async () => {
+    const something = await fnAsync();
+    console.log(something);
+    console.log('Hello');
+};
+
+console.log('Before');
+anotherFunction();
+console.log('After');
+
+
+//Before, after, hello async world, hello
